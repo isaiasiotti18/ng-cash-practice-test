6e66b732e5573de2579a5fdd06b64c850baa5511
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-11-16 20:42:58 -0300

    Swagger Api

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('NG CASH PRACTICE TEST')
+    .setDescription('The NG CASH PRACTICE TEST API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('NG_CASH_PRACTICE_TEST')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
