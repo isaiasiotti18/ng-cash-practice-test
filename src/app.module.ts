1bd50cf0da82466d68dcd90e9f3e83747c82de7c
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-11-21 19:54:26 -0300

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { typeormAsyncConfig } from './shared/database/typeorm/typeorm.config';
 import { UsersModule } from './modules/users/users.module';
 import { AccountsModule } from './modules/accounts/accounts.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './shared/auth/guards/jwt-auth.guard';
+import { AuthModule } from './shared/auth/auth.module';
 
 @Module({
   imports: [
@@ -15,8 +18,14 @@ import { TransactionsModule } from './modules/transactions/transactions.module';
     UsersModule,
     AccountsModule,
     TransactionsModule,
+    AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
